dc7eada8d7ffe47f08be6aa6c351b52b3f993cb1
abhimanyu-stream <abhimanyu.intelligence@gmail.com>
2024-10-24 17:34:08 +0530

    String reverse program added

---
+++ src/main/java/com/java17/interview/prepartion/WhatIsOutPut.java
@@ -0,0 +1,19 @@
+package com.java17.interview.prepartion;
+
+public class WhatIsOutPut {
+
+    public static void main(String[] args) {
+
+        System.out.println(10 + 20 +"Interview");
+        System.out.println("Interview" + 10 + 20);
+        // finally block will be skipped if System.exit(0); is executed in try{}
+        try{
+            System.exit(0);
+        }catch (Exception e){
+
+        }finally {
+            System.out.println("finally");
+        }
+    }
+
+}
